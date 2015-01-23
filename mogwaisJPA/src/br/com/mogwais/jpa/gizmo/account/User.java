7c25fb6084d5813977a056eee5f978e5a2e71000
PauloFerreira25 <paulo.ferreira25@gmail.com>
2015-01-23 20:34:27 -0200

    JPA

---
+++ mogwaisJPA/src/br/com/mogwais/jpa/gizmo/account/User.java
@@ -0,0 +1,45 @@
+package br.com.mogwais.jpa.gizmo.account;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -1965223765862567357L;
+
+	// Private ID
+	@Id
+	private Long userId;
+
+	// Public ID
+	@Column(unique = true)
+	private String hashUser;
+
+	// Security
+	@Column
+	private String salt;
+
+	// Auth
+	@Column(unique = true)
+	private String email;
+
+	@Column
+	private String password;
+
+	// User Information
+	@NotNull()
+	@Column
+	private String fullName;
+
+	@Column
+	@Temporal(TemporalType.DATE)
+	private Date birthday;
+}
---
+++ mogwaisJPA/src/br/com/mogwais/jpa/gizmo/account/UserPK.java
@@ -0,0 +1,16 @@
+package br.com.mogwais.jpa.gizmo.account;
+
+import java.io.Serializable;
+
+public class UserPK implements Serializable{
+
+
+	private static final long serialVersionUID = -6034188419893790468L;
+	
+	// 19850125PAAF______1234567890
+	// YYYYMMDDXXXXXXXXXX+RANDON(10)
+	private String userID;
+	
+	
+	
+}
